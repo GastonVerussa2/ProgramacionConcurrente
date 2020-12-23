2d334e98802b4eeeddb0d5ba4d83762a87117592
gastonverussa <sebas@192.168.0.15>
2020-12-23 11:46:21 -0300

    Hice los métodos requeridos del recurso compartido.

--- ProgramacionConcurrente/src/main/java/TP6_Ej2/GestorSala.java
+++ ProgramacionConcurrente/src/main/java/TP6_Ej2/GestorSala.java
@@ -11,4 +11,52 @@ package TP6_Ej2;
  */
 public class GestorSala {
     
+    private int cantidadPersonas = 0;
+    private int capacidad;
+    private int jubiladosEsperando = 0;
+    private final int tUmbral;
+    
+    public GestorSala(int capacidad, int umbralTemperatura){
+        this.capacidad = capacidad;
+        tUmbral = umbralTemperatura;
+    }
+    
+    public synchronized void entrarSala(){
+        // se invoca cuando una persona quiere entrar en la sala.
+        try{
+            while((cantidadPersonas >= capacidad)||(jubiladosEsperando > 0)){
+                this.wait();
+            }
+            cantidadPersonas++;
+        } catch(InterruptedException E){
+        }
+    }
+    
+    public synchronized void entrarSalaJubilado(){
+        // se invoca cuando una persona jubilada quiere entrar en la sala.
+        try{
+            jubiladosEsperando++;
+            while(cantidadPersonas >= capacidad){
+                this.wait();
+            }
+            cantidadPersonas++;
+            jubiladosEsperando--;
+        } catch(InterruptedException E){
+        }
+    }
+    
+    public synchronized void salirSala(){
+        // se invoca cuando una persona, jubilada o no, quiere salir de la sala
+        cantidadPersonas--;
+        this.notifyAll();
+    }
+    
+    public synchronized void notificarTemperatura(int temperatura){
+        // lo invoca la hebra que mide la temperatura de la sala para indicar el último valor medido
+        if(temperatura > tUmbral){
+            capacidad = 35;
+        } else {
+            capacidad = 50;
+        }
+    }
 }
