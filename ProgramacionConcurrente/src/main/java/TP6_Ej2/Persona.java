2f25ed40c09ae6ba515270cbf7230fab03663bd1
gastonverussa <sebas@192.168.0.15>
2020-12-23 12:43:52 -0300

    Junté los jubilados y no jubilados en la clase Persona y los diferencio con un boolean. Completé las clases Persona y Termometro.

---
+++ ProgramacionConcurrente/src/main/java/TP6_Ej2/Persona.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP6_Ej2;
+
+/**
+ *
+ * @author sebas
+ */
+public class Persona extends Thread{    //Hilo para simular a las personas que van al museo.
+    
+    private final GestorSala SalaMuseo; //Recurso compartido.
+    private final boolean jubilado;     //Boolean para saber si es o no jubilado.
+    private final String nombre;        //Variable para guardar el nombre y acceder al mismo más facilmente.
+    
+    public Persona(GestorSala SalaMuseo, boolean jubilado, String nombre){  //Metodo contructor, recibe recurso, si es jubilado y nombre.
+        this.SalaMuseo = SalaMuseo;
+        this.jubilado = jubilado;
+        if(jubilado){                               //Define su nombre dependiendo si es jubilado o no.
+            this.nombre = "Jubilado " + nombre;
+        } else {
+            this.nombre = "Visitante " + nombre;
+        }
+        this.setName(this.nombre);
+    }
+    
+    private void caminarAlMuseo(){              //Método para simular que camina al museo y separar la llegada de cada persona.
+        try{
+            Thread.sleep((int) (Math.random() * 30000));
+        } catch(InterruptedException E){
+            
+        }
+    }
+    
+    private void pasearSala(){              //Método para simular que pasea dentro de la sala.
+        try{
+            Thread.sleep(5000 + (int) (Math.random() * 7500));
+        } catch(InterruptedException E){
+            
+        }
+    }
+    
+    public void run(){          //Método run.
+        this.caminarAlMuseo();
+        System.out.println(nombre + ": Llegué al museo.");      //Carteles para hacer seguimiento.
+        if(jubilado){                                           //If para diferenciar que metodo de entrada debe usar.
+            SalaMuseo.entrarSalaJubilado();                     //Luego usa su respectivo método para entrar.
+        } else {
+            SalaMuseo.entrarSala();
+        }
+        System.out.println("--" + nombre + ": Logré entrar a la sala.");
+        this.pasearSala();
+        SalaMuseo.salirSala();                                 //Sale de la sala.
+        System.out.println("----" + nombre + ": Salí de la sala.");
+    }
+}
--- ProgramacionConcurrente/src/main/java/TP6_Ej2/Termometro.java
+++ ProgramacionConcurrente/src/main/java/TP6_Ej2/Termometro.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,6 +9,32 @@ package TP6_Ej2;
  *
  * @author sebas
  */
-public class Termometro {
+public class Termometro extends Thread{             //Hilo que sirve para medir la temperatura y notificar al gestor.
+              
+    private final int espera;                       //Variable para modificar más facilmente la espera entre medida y medida.
+    private final GestorSala SalaMuseo;             //Recurso compartido
     
+    public Termometro(GestorSala sala, int espera){ //Metodo constructor, recibe como parametro el recurso compartido y la espera.
+        SalaMuseo = sala;
+        this.espera = espera;
+    }
+    
+    private void esperar(){                 //Espera entre medida y medida
+        try{
+            Thread.sleep(espera);
+        } catch(InterruptedException E){
+            
+        }
+    }
+    
+    private int medirTemperatura(){                     //Metodo que simula la medición de la temperatura.
+        return 20000 + (int) (Math.random() * 15000);
+    }
+    
+    public void run(){                                                  //Metodo run.
+        while(true){                                                    //Repite indefinidamente.
+            SalaMuseo.notificarTemperatura(this.medirTemperatura());    //Mide la temperatura y le notifica al gestor.
+            this.esperar();                                             //Espera para volver a medir.
+        }
+    }
 }
